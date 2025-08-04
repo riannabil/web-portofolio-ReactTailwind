11a4de2e8e2c481336dd927b03ea23b940c5653e
riannabil <riannabil104@gmail.com>
2025-08-04 20:43:27 +0700

    Navbar Commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro numquam
+        iste sapiente nulla adipisci itaque voluptate doloremque voluptas? Iste
+        molestias similique ducimus atque quo impedit perspiciatis recusandae ea
+        exercitationem provident in officia placeat, expedita, consequatur ipsum
+        maxime voluptas dicta. Debitis ea tempore ipsam fugiat in amet
+        reprehenderit eveniet maiores qui et soluta numquam voluptas omnis
+        molestiae sequi, dicta suscipit accusantium. Ducimus, vitae repellendus
+        eius tenetur saepe beatae aliquam inventore, ratione, qui et similique
+        rem. Possimus expedita optio beatae. Sequi velit eligendi nesciunt
+        aspernatur error ab dolore, rem, eum deserunt inventore ea molestias ex
+        alias ipsa, atque repellat aliquam quia nulla perferendis assumenda
+        officiis veniam! Repellendus saepe autem beatae maiores placeat magnam
+        aliquam nam, voluptate animi eum perferendis sequi quod repellat.
+        Corporis itaque quia laudantium veritatis quibusdam, culpa nisi veniam
+        corrupti ea voluptatem quas porro praesentium temporibus expedita odit
+        fugiat suscipit facere. Asperiores quas perspiciatis voluptatibus magnam
+        soluta, consequatur ratione cumque ipsam excepturi quisquam adipisci aut
+        natus. Eius possimus quibusdam vel reiciendis, veniam nihil ut magni
+        tempora quos, impedit, dolorum itaque facilis distinctio. Perferendis,
+        ipsam esse error veniam nemo possimus neque consequuntur, cupiditate
+        doloribus, nobis blanditiis! Accusantium est, numquam fugiat alias,
+        natus architecto deserunt dignissimos libero in quo sequi tempore minus!
+      </h1>
+    </>
+  );
+}
+
+export default App;
