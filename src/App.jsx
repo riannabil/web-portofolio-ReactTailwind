ffd511679e5b3a44e77f2e5a0043b92cbf78e1a8
riannabil <riannabil104@gmail.com>
2025-08-07 23:40:03 +0700

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,52 @@
+import DataImage from "./data";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro numquam
-        iste sapiente nulla adipisci itaque voluptate doloremque voluptas? Iste
-        molestias similique ducimus atque quo impedit perspiciatis recusandae ea
-        exercitationem provident in officia placeat, expedita, consequatur ipsum
-        maxime voluptas dicta. Debitis ea tempore ipsam fugiat in amet
-        reprehenderit eveniet maiores qui et soluta numquam voluptas omnis
-        molestiae sequi, dicta suscipit accusantium. Ducimus, vitae repellendus
-        eius tenetur saepe beatae aliquam inventore, ratione, qui et similique
-        rem. Possimus expedita optio beatae. Sequi velit eligendi nesciunt
-        aspernatur error ab dolore, rem, eum deserunt inventore ea molestias ex
-        alias ipsa, atque repellat aliquam quia nulla perferendis assumenda
-        officiis veniam! Repellendus saepe autem beatae maiores placeat magnam
-        aliquam nam, voluptate animi eum perferendis sequi quod repellat.
-        Corporis itaque quia laudantium veritatis quibusdam, culpa nisi veniam
-        corrupti ea voluptatem quas porro praesentium temporibus expedita odit
-        fugiat suscipit facere. Asperiores quas perspiciatis voluptatibus magnam
-        soluta, consequatur ratione cumque ipsam excepturi quisquam adipisci aut
-        natus. Eius possimus quibusdam vel reiciendis, veniam nihil ut magni
-        tempora quos, impedit, dolorum itaque facilis distinctio. Perferendis,
-        ipsam esse error veniam nemo possimus neque consequuntur, cupiditate
-        doloribus, nobis blanditiis! Accusantium est, numquam fugiat alias,
-        natus architecto deserunt dignissimos libero in quo sequi tempore minus!
-      </h1>
+      <div className="hero md:grid grid-cols-2 items-center pt-10 xl:gap-0 gap-6 grid-cols-1">
+        <div>
+          <div className="flex items-center gap-3 mb-6 bg-zinc-800 w-fit p-4 rounded-2xl">
+            <img
+              src={DataImage.HeroImage}
+              alt="Hero Image"
+              className="w-10  rounded-md"
+            />
+            <q>
+              Jika kamu terpaku oleh sebuah hasil maka itu akan membuat mu tidak
+              bersyukur, Tapi jika kamu berfokus kepada proses maka kamu akan
+              menghargai sekecil apapun hasilnya
+            </q>
+          </div>
+          <h1 className="text-5xl/tight font-bold mb-6">
+            Hi!👋, Saya Muhammad Rian Nabil Maulana
+          </h1>
+          <p className="text-base/loose mb-6 opacity-50">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi,
+            possimus facilis nam cumque id adipisci quam nemo provident veniam
+            fugit! Nobis alias ullam quis numquam tenetur facere omnis odio
+            quod.
+          </p>
+          <div className="flex items-center sm:gap-4 gap-2">
+            <a
+              href="#"
+              className="bg-violet-700 p-4 rounded-2xl hover:bg-violet-600"
+            >
+              Download CV <i className="ri-download-line ri-lg"></i>
+            </a>
+            <a
+              href="#"
+              className="bg-zinc-700 p-4 rounded-2xl hover:bg-zinc-600"
+            >
+              Lihat Project <i className="ri-arrow-down-line ri-lg"></i>
+            </a>
+          </div>
+        </div>
+        <img
+          src={DataImage.HeroImage}
+          alt="Hero Image"
+          className="w-[500px] md:ml-auto"
+        />
+      </div>
     </>
   );
 }
